b1a6378b6c69ec05773f00d26164ea0161abf760
ks <francisco_larrahona@hotmail.com>
2024-09-02 23:09:56 -0300

    testing

--- test/carts.routes.test.js
+++ test/carts.routes.test.js
@@ -1,22 +1,56 @@
 import * as chai from 'chai';
+
 import supertest from 'supertest';
 
 const expect = chai.expect;
 
 const requester = supertest('http://localhost:8080');
-const testUser = {first_name: 'pedro', last_name: 'perez', email:'pedro.perez@hotmail.com', password: '1234'};
+const testUser = {
+
+first_name: 'pedro',
+
+last_name: 'perez',
+
+email: 'pedro.perez@hotmail.com',
+
+password: '1234'
+
+};
+
+const testProduct = {
+
+nombre: 'producto de prueba',
+
+categoria: 'categoria de producto de prueba',
+
+precio: 99,
+
+descripcion: 'descripcion de producto de prueba',
+
+stock: 999
+}
+
 
 describe('test integracion del carrito de compras', function(){
     it('GET /api/carts/ debe mostrar los diferentes carritos', async function() {
-        const {_body} = await requester.get('/api/carts');
+        try{
+        const {body} = await requester.get('/api/carts');
 
-        expect(_body.error).to.be.undefined;
-        expect(_body.payload).to.be.ok
+        expect(body.error).to.be.undefined;
+        expect(body.payload).to.be.ok
+
+        }catch (error){
+            console.error('Error fetching carts:', error);
+        }
     })
     it('DELETE /api/carts/cid/products debe eliminar el carrito seleccionado envase a su id', async function(){
-        const{_body} = await requester.get('/api/carts/665df710bd11eab4d696de60/products')
+        try{
+        const{body} = await requester.get('/api/carts/665df710bd11eab4d696de60/products')
         
-        expect(_body.error).to.be.undefined;
-        expect(_body.payload).to.be.ok
+        expect(body.error).to.be.undefined;
+        expect(body.payload).to.be.ok
+        } catch(error){
+            console.error('error deleting cart:', error);
+        }
     } )
 })
--- test/sessions.routes.test.js
+++ test/sessions.routes.test.js
@@ -1,27 +1,62 @@
 import * as chai from 'chai';
+
 import supertest from 'supertest';
 
 const expect = chai.expect;
 
 const requester = supertest('http://localhost:8080');
-const testUser = {first_name: 'pedro', last_name: 'perez', email:'pedro.perez@hotmail.com', password: '1234'};
-let cookie = {};
+const testUser = {
+
+first_name: 'pedro',
+
+last_name: 'perez',
+
+email: 'pedro.perez@hotmail.com',
+
+password: '1234'
+
+};
+
+const testProduct = {
+
+nombre: 'producto de prueba',
+
+categoria: 'categoria de producto de prueba',
+
+precio: 99,
+
+descripcion: 'descripcion de producto de prueba',
+
+stock: 999
+
+};
+const cookie = {}
+
 
 describe('Test integracion de sessions', function(){
     it('POST /api/sessions/register debe poder registrar un nuevo usuario', async function() {
-        const {_body} = (await requester.post('/api/sessions/register')).setEncoding(testUser);
+        try{
+        const {body} = (await requester.post('/api/sessions/register')).setEncoding(testUser);
         
-        expect(_body.error).to.be.undefined;
-        expect(_body.payload).to.be.ok;
+        expect(body.error).to.be.undefined;
+        expect(body.payload).to.be.ok;
+        }catch(error){
+            console.error('error : user registration error', error);
+        }
     });
     it('POST /api/sessions/login debe poder logear el usuario correctamente', async function() {
-        const result = (await requester.post('/api/sessions/login')).setEncoding(testUser);
-        const cookieData = result.headers['set-cookie'][0];
-        cookie = {name: cookieData.split('=')[0], value: cookieData.split('=')[1]};
+        try{
+            const result = (await requester.post('/api/sessions/login')).setEncoding(testUser);
+            const cookieData = result.headers['set-cookie'][0];
+            cookie = {name: cookieData.split('=')[0], value: cookieData.split('=')[1]};
+    
+            expect(cookieData).to.be.ok;
+            expect(cookie.name).to.be.equals('coderCookie');
+            expect(cookie.value).to.be.ok  
+        }catch(error){
+            console.error('error : error logging in user', error);
+        }
 
-        expect(cookieData).to.be.ok;
-        expect(cookie.name).to.be.equals('coderCookie');
-        expect(cookie.value).to.be.ok  
     });
 
 })
